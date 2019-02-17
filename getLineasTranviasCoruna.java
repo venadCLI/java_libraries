8a1a983a53b80bd811227c8e92bbaef041de4d70
André López <44867242+venadCLI@users.noreply.github.com>
2019-02-17 20:49:17 +0100

    Update getLineasTranviasCoruna.java

--- getLineasTranviasCoruna.java
+++ getLineasTranviasCoruna.java
@@ -1,22 +1,30 @@
-private ArrayList<String> getLineasTranviasCoruna() {
+/**
+ * <p>Returns every line from tranviascoruna.com website throught web parsing
+ * using Jsoup.</p>
+ *
+ * @return ArrayList of Strings with the lines on the website
+ * @exception
+ * @throws IOException if website cannot be processed correctly
+ * @
+ */
+private ArrayList<String> getLineasTranviasCoruna() throws IOException {
     ArrayList<String> lineasAutobus = new ArrayList<>();
     String url_lineas = "http://www.tranviascoruna.com/lineas-y-horarios/";
-    try {
-        //Start party.
-        Document doc = Jsoup.connect(url_lineas).get();
-        Elements a = doc.select("a[href]");
-        for (Element e : a) {
-            String enlace = e.attr("href");
-            String urlBase = "http://www.tranviascoruna.com/lineas-y-horarios/?linea=";
-            if (enlace.contains(urlBase)) {
-                String[] linea = enlace.split("=");
-                if (linea[0].endsWith("linea") && !lineasAutobus.contains(linea[1])) {
-                    lineasAutobus.add(linea[1]);
-                }
+    //Start party.
+    Document doc = Jsoup.connect(url_lineas).get();
+    Elements a = doc.select("a[href]");
+    for (Element e : a) {
+        String enlace = e.attr("href");
+        String urlBase = "http://www.tranviascoruna.com/lineas-y-horarios/?linea=";
+        if (enlace.contains(urlBase)) {
+            String[] linea = enlace.split("=");
+            if (linea[0].endsWith("linea") && !lineasAutobus.contains(linea[1])) {
+                lineasAutobus.add(linea[1]);
             }
         }
-    } catch (IOException ex) {
-        System.out.println(ex.getMessage());
+    }
+    if (lineasAutobus.size() == 0) {
+        throw new IOException("Arraylist is empty, there's a problem with website");
     }
     return lineasAutobus;
 }
